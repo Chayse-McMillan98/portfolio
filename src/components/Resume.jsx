2e7a379a9489727c0b005326668a84cb97b04134
Chayse McMillan <chaysemcmillan@reciprotech.lan>
2023-10-10 15:42:47 -0600

    added resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Title from './Title';
+import { AiOutlineDownload } from 'react-icons/ai';
 import backgroundImg from '../assets/images/sawtooth_3.jpg';
+import resume from '../assets/files/resume.pdf';
+import resume_1 from '../assets/images/resume_1.jpg';
+import resume_2 from '../assets/images/resume_2.jpg';
 
 const Resume = () => {
     return (
@@ -21,7 +25,46 @@ const Resume = () => {
             ></Title>
 
             { /* Project Items */}
-            <div className='h-screen'>
+            <div className='flex flex-nowrap justify-center'>
+                <div>
+                    <div className='relative md:px-14 px-8 md:pb-14 pb-8'>
+                        <img className='object-contain rounded-2xl' src={resume_1} alt="logo"/>
+                    </div>
+                    <div className='relative md:px-14 px-8 md:pb-8 pb-4'>
+                        <img className='object-contain rounded-2xl' src={resume_2} alt="logo"/>
+                    </div>
+
+                    <div className='relative md:pb-8 pb-4'>
+                        { /* Large Device Button*/ }
+                        <div className='md:block hidden'>
+                            <div className='justify-center flex'>
+                                <a href={resume} rel="noreferrer" className='bg-black text-white mt-4 w-1/2 p-4 rounded-lg' download="Chayse_McMillan_Resume.pdf" target='_blank'>
+                                    <div className='justify-center flex'>
+                                        <div className='pr-2 MontserratBoldFont'>Download</div>
+                                        <AiOutlineDownload className='text-white mt-1' size={20}/>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        { /* Small Device Button*/ }
+                        <div className='md:hidden block'>
+                            <div className='justify-center flex'>
+                                <a href={resume} rel="noreferrer" className='bg-black text-white mt-4 w-1/2 p-4 rounded-lg' download="Chayse_McMillan_Resume.pdf" target='_blank'>
+                                    <div className='justify-center flex'>
+                                        <div className='pr-2 MontserratBoldFont'>Download</div>
+                                        <AiOutlineDownload className='text-white mt-1' size={20}/>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+
             </div>   
 
         </div>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,20 +4,20 @@ const Title = ({ text, largeDeviceUnderline, smallDeviceUnderline }) => {
   return (
     <div>
         <div className='flex justify-center w-full'>
-            <div className="relative py-16">
-                <h2 className='MontserratBoldFont text-center text-black md:text-7xl text-5xl'>{text}</h2>
+            <div className="relative md:py-16 py-8">
+                <h2 className='MontserratBoldFont text-center text-white md:text-7xl text-5xl'>{text}</h2>
                 <div className="py-2">
 
                     { /* Large Device Underline */}
                     <div className='md:flex hidden' >
-                        <div className='bg-gradient-to-l from-black' style={{height: '2px', width: largeDeviceUnderline}}></div>
-                        <div className='bg-gradient-to-r from-black' style={{height: '2px', width: largeDeviceUnderline}}></div>
+                        <div className='bg-gradient-to-l from-white' style={{height: '2px', width: largeDeviceUnderline}}></div>
+                        <div className='bg-gradient-to-r from-white' style={{height: '2px', width: largeDeviceUnderline}}></div>
                     </div>
 
                     { /* Small Device Underline */}
                     <div className='md:hidden flex' >
-                        <div className='bg-gradient-to-l from-black' style={{height: '2px', width: smallDeviceUnderline}}></div>
-                        <div className='bg-gradient-to-r from-black' style={{height: '2px', width: smallDeviceUnderline}}></div>
+                        <div className='bg-gradient-to-l from-white' style={{height: '2px', width: smallDeviceUnderline}}></div>
+                        <div className='bg-gradient-to-r from-white' style={{height: '2px', width: smallDeviceUnderline}}></div>
                     </div>
                 </div>
             </div>
